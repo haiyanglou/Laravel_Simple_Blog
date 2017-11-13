80312c7f05110c12eeacb2f40d260ddeb903ddb7
haiyanglou <>
2017-11-13 13:39:35 -0600

    first commit

---
+++ app/Helpers/html.php
@@ -0,0 +1,36 @@
+<?php
+
+# style tag with time stamp
+
+function style_ts($path) {
+
+    try
+    {
+        $ts = '?v=' . File:: lastModified(public_path().$path);
+    }
+
+    catch (Exception $e)
+    {
+        $ts = '';
+    }
+
+    return '<link media= "all" type="text/css" rel="stylesheet" herf="' . $path . $ts . '">';
+}
+
+
+# screen tag with time stamp 
+
+function script_ts($path) {
+
+    try
+    {
+        $ts = '?v=' . File:: lastModified(public_path().$path);
+    }
+
+    catch (Exception $e)
+    {
+        $ts = '';
+    }
+
+    return '<script src="' . $path . $ts . '"></script>';
+}
