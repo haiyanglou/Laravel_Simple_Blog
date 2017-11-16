1eaa6e0351ee240b2315287dfd314e3e73b141da
haiyanglou <>
2017-11-16 01:24:35 -0600

    login/register done

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,16 @@
 <?php
 
 Route::get('/', 'IndexController@index');
-
-
+/*
+Route::get('/', function () {
+    return view('welcome');
+});
+*/
 Auth::routes();
 
-Route::get('/home', 'HomeController@home')->name('home');
+Route::get('/home', 'HomeController@index')->name('home');
 Route::get('/admin', 'AdminController@index')->name('admin');
+/*
+Route::get('/home', 'HomeController@index');
+Route::get('/admin', 'AdminController@index');
+*/
